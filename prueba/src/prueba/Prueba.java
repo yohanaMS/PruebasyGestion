b14b8a980d2a9c375b2ea1f138c39014bec4fa04
Yoha <Yoha@Yoha>
2017-11-19 14:46:08 -0500

    Comit flujo de prueba

--- prueba/src/prueba/Prueba.java
+++ prueba/src/prueba/Prueba.java
@@ -5,7 +5,9 @@
  */
 package prueba;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
@@ -14,29 +16,60 @@ import org.openqa.selenium.firefox.FirefoxDriver;
  * @author Yoha
  */
 public class Prueba {
-    
-    private static WebDriver driverC=null;
-    
-    private static WebDriver driverF=null;
+
+    private static WebDriver driverC = null;
+    private static WebDriver driverF = null;
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.setProperty("webdriver.chrome.driver","C:\\Users\\Yoha\\Downloads\\chromedriver.exe");
-        
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Yoha\\Downloads\\chromedriver.exe");
+        WebElement search;
+        WebElement button;
+        WebElement addCar;
+        WebElement checkout;
+
         driverC = new ChromeDriver();
         driverC.get("http:\\automationpractice.com/index.php");
+
+        search = driverC.findElement(By.id("search_query_top"));
+        search.click();
+        search.sendKeys("chiffon dress");
+        search.submit();
+
+        button = driverC.findElement(By.className("ajax_add_to_cart_button"));
+        button.click();
+
+        // addCar = driverC.findElement(By.xpath("div[contains(@class,'button-container')/span/span"));
+        // addCar.click();
+        // addCar = driverC.findElement(By.className("cross"));
+        //addCar.click();
+//        
+        search = driverC.findElement(By.id("search_query_top"));
+        search.click();
+        search.clear();
+        search.sendKeys("Blouse");
+        search.submit();
         
-       // driverC.
+        button = driverC.findElement(By.className("ajax_add_to_cart_button"));
+        button.click();
+         
         
-       // driverF = new FirefoxDriver();
+        search = driverC.findElement(By.id("search_query_top"));
+        search.click();
+        search.clear();
+        search.submit();
+        
+        checkout = driverC.findElement(By.className("shopping_cart"));
+        checkout.click();
+
+        checkout = driverC.findElement(By.id("center_column"));
+        checkout.click();
+        // driverC.
+        // driverF = new FirefoxDriver();
         //driverF.get("http:\\automationpractice.com/index.php");
-        
-        
-        
-        
     }
-    
+
 }
